086e068c1464cc3694afade8310c950eb6352ac6
mehta-g1 <vikash.kumarmehta.bca.2023@mitmeerut.ac.in>
2025-08-22 11:55:09 +0530

    database created

--- Hotel/Order/models.py
+++ Hotel/Order/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Categories(models.Model):
+    category_name = models.CharField(max_length=100)
+    added_date = models.TimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.category_name
+
+class Dishes(models.Model):
+    ch = [("SG", "Signature"), ("SP", "Spicy"), ("SP", "Special")]
+    category = models.ForeignKey(Categories, on_delete=models.CASCADE)
+    dish_name = models.CharField(max_length=150)
+    receipe = models.TextField(blank=True, null=True)
+    price = models.IntegerField()
+    optional = models.CharField(choices=ch, null=True, blank=True, max_length=100)
+
+    def __str__(self):
+        return f"{self.dish_name} - {self.category}"
+
+
+
